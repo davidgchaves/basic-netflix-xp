b9b53a426d99d004de98dbd4f170b6abbf226861
David G Chaves <davidgchaves@gmail.com>
2016-03-11 11:51:57 +0100

    <Search /> renders all available shows spec

--- test/App.spec.js
+++ test/App.spec.js
@@ -4,10 +4,17 @@ import { expect } from 'chai'
 import { shallow } from 'enzyme'
 
 import Search from '../src/Search'
+import ShowCard from '../src/ShowCard'
+import data from '../public/data'
 
 describe('<Search />', () => {
   it('renders our brand', () => {
     const wrapper = shallow(<Search />)
     expect(wrapper.contains(<h1 className='brand'>Basic Netflix XP</h1>)).to.be.true
+  })
+
+  it('renders all available shows', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find(ShowCard).length).to.equal(data.shows.length)
   })
 })
