4af05ffdea0ea6a1ec0548b7e9b40ccb4e890c5c
David G Chaves <davidgchaves@gmail.com>
2016-03-11 11:11:05 +0100

    Add search box header to find the show you want

--- src/Search.jsx
+++ src/Search.jsx
@@ -3,14 +3,48 @@ import React from 'react'
 import data from '../public/data'
 import ShowCard from './ShowCard'
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {data.shows.map((show) => (
-        <ShowCard {...show} key={show.imdbID} />
-       ))}
-    </div>
-  </div>
-)
+class Search extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { searchTerm: '' }
+
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
+  }
+
+  handleSearchTermChange (event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
+  render () {
+    const { searchTerm } = this.state
+
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>Search Shows</h1>
+          <input
+            type='text'
+            className='search-input'
+            placeholder='Search'
+            value={searchTerm}
+            onChange={this.handleSearchTermChange}
+          />
+        </header>
+        <div className='shows'>
+          {data.shows
+            .filter((show) =>
+              `${show.title} ${show.description}`
+                .toUpperCase()
+                .indexOf(searchTerm.toUpperCase()) >= 0
+            )
+            .map((show) =>
+              <ShowCard {...show} key={show.imdbID} />
+           )}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Search
