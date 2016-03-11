56074bf69574691d3465a93962ea2237911ad38d
David G Chaves <davidgchaves@gmail.com>
2016-03-11 09:49:12 +0100

    Extract ShowCard presentational

---
+++ src/ShowCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ShowCard = ({ description, poster, title, year }) => (
+  <div className='show'>
+    <img src={`public/img/posters/${poster}`} className='show-card-img' />
+    <div className='show-text'>
+      <h3 className='show-title'>{title}</h3>
+      <h4 className='show-year'>{year}</h4>
+      <p className='show-description'>{description}</p>
+    </div>
+  </div>
+)
+
+ShowCard.propTypes = {
+  description: React.PropTypes.string.isRequired,
+  poster: React.PropTypes.string.isRequired,
+  title: React.PropTypes.string.isRequired,
+  year: React.PropTypes.string.isRequired
+}
+
+export default ShowCard
