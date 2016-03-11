28d3601fe539f8d17e6529a7ff496a8652ecae14
David G Chaves <davidgchaves@gmail.com>
2016-03-11 08:52:31 +0100

    Enter `react-router` (TODO: Use `browserHistory`)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Router, Route, hashHistory } from 'react-router'
 
 import Landing from './Landing'
 
 const App = () => (
-  <Landing />
+  <Router history={hashHistory}>
+    <Route path='/' component={Landing} />
+  </Router>
 )
 
 export default App
