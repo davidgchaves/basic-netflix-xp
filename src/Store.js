aa0a50c9a43dd33ab1d4dd4d29fbeee1499db6f2
David G Chaves <davidgchaves@gmail.com>
2016-03-15 10:08:07 +0100

    Wire up Redux DevTools

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,20 @@
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 
 import reducer from './Reducer'
+import { INITIAL_STATE } from './Constants'
 
-export default createStore(reducer)
+const reduxDevTools = () => (
+  (typeof window === 'object' &&
+   typeof window.devToolsExtension !== 'undefined'
+  )
+    ? window.devToolsExtension()
+    : (f) => f
+)
+
+const store = createStore(
+  reducer,
+  INITIAL_STATE,
+  compose(reduxDevTools())
+)
+
+export default store
