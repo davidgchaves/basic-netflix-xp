736ef25470e73190d0d825f991189c31b7d3cf58
David G Chaves <davidgchaves@gmail.com>
2016-03-15 12:41:36 +0100

    Isolate `window` and `document` from the rest of the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import { Provider } from 'react-redux'
 
 import Landing from './Landing'
 import Layout from './Layout'
 import Search from './Search'
 import Details from './Details'
+import store from './Store'
 
 class App extends React.Component {
   render () {
     return (
-      <Router history={hashHistory}>
-        <Route path='/' component={Layout} >
-          <IndexRoute component={Landing} />
-          <Route path='/search' component={Search} />
-          <Route path='/details/:id' component={Details} />
-        </Route>
-      </Router>
+      <Provider store={store}>
+        <Router history={hashHistory}>
+          <Route path='/' component={Layout} >
+            <IndexRoute component={Landing} />
+            <Route path='/search' component={Search} />
+            <Route path='/details/:id' component={Details} />
+          </Route>
+        </Router>
+      </Provider>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
 
 import App from './App'
-import store from './Store'
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('app')
 )
