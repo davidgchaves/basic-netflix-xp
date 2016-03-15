569671a447ce7e9f9f34fd6fc1ed6450e012beb5
David G Chaves <davidgchaves@gmail.com>
2016-03-15 11:19:34 +0100

    Add Store Reducer tests

--- test/App.spec.js
+++ test/App.spec.js
@@ -7,7 +7,8 @@ import { Provider } from 'react-redux'
 import Search from '../src/Search'
 import Header from '../src/Header'
 import store from '../src/Store'
-import { SET_SEARCH_TERM } from '../src/Constants'
+import reducer from '../src/Reducer'
+import { SET_SEARCH_TERM, INITIAL_STATE } from '../src/Constants'
 import data from '../public/data'
 
 describe('<Header />', () => {
@@ -39,5 +40,47 @@ describe('<Search />', () => {
     )
 
     expect(wrapper.find('div.show-card').length).to.equal(2)
+  })
+})
+
+describe('Store reducer', () => {
+  context('when receiving an undefined state', () => {
+    const undefinedState = undefined
+
+    it('defaults to the INITIAL_STATE', () => {
+      const whateverAction = { payload: 'WhaT3v3r' }
+
+      expect(
+        reducer(undefinedState, whateverAction)
+      ).to.eql(INITIAL_STATE)
+    })
+  })
+
+  context('when an unknown action is dispatched', () => {
+    const unknownAction = { payload: 'UnKnoWN ActIon' }
+
+    it('returns the passed in state', () => {
+      const stateBefore = { searchTerm: 'House' }
+
+      expect(
+        reducer(stateBefore, unknownAction)
+      ).to.eql(stateBefore)
+    })
+  })
+
+  context('when a SET_SEARCH_TERM action is dispatched', () => {
+    const setSearchTermAction = {
+      type: 'SET_SEARCH_TERM',
+      payload: 'my search string'
+    }
+
+    it('updates the search term accordingly', () => {
+      const stateBefore = { searchTerm: 'House' }
+      const stateAfter = { searchTerm: 'my search string' }
+
+      expect(
+        reducer(stateBefore, setSearchTermAction)
+      ).to.eql(stateAfter)
+    })
   })
 })
