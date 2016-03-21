b02d311050c41eb92c8e4920a17b1ddcbab07138
David G Chaves <davidgchaves@gmail.com>
2016-03-21 18:07:24 +0100

    Get IMDB ratings from Client dinamically

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { get } from 'axios'
 
 import Header from './Header'
 import SearchContainer from './SearchContainer'
 
 class Details extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { omdbData: {} }
+  }
+
+  componentDidMount () {
+    const { id } = this.props.params
+
+    get(`http://www.omdbapi.com/?i=${id}`)
+      .then(({ data }) => this.setState({ omdbData: data }))
+      .catch((error) => console.log(`Axios Error: ${error}`))
+  }
+
   assignShow (shows, id) {
     const show = shows.filter((s) => s.imdbID === id)
 
@@ -12,10 +27,17 @@ class Details extends React.Component {
     return show[0] || {}
   }
 
+  renderRating (imdbRating) {
+    if (imdbRating) {
+      return <h3 className='video-rating'>{imdbRating}</h3>
+    }
+  }
+
   render () {
     const { shows } = this.props
     const { id } = this.props.params
     const { description, poster, title, trailer, year } = this.assignShow(shows, id)
+    const { imdbRating } = this.state.omdbData
     const youtubeUrl = `https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`
     const posterUrl = `/public/img/posters/${poster}`
 
@@ -26,6 +48,7 @@ class Details extends React.Component {
         <div className='video-info'>
           <h1 className='video-title'>{title}</h1>
           <h2 className='video-year'>({year})</h2>
+          {this.renderRating(imdbRating)}
           <img className='video-poster' src={posterUrl} />
           <p className='video-description'>{description}</p>
         </div>
